c0d7c50f4822a6066d3da57d1f9e2ee219a4fe88
Rolf K <abstractstar@gmail.com>
2025-02-07 21:43:22 +0100

    Fix update() in countries controller

--- src/controllers/countries.rs
+++ src/controllers/countries.rs
@@ -13,7 +13,7 @@ pub struct Params {
 
 impl Params {
     fn update(&self, item: &mut ActiveModel) {
-        item.name = Set(self.name.clone());
+        item.name = Set(self.name.clone().unwrap());
     }
 }
 
