cca639d178746181db06ebce00e53336a8c79777
Rolf K <abstractstar@gmail.com>
2025-02-23 13:01:30 +0100

    Add slug column to countries table

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -15,6 +15,7 @@ mod m20250211_175304_songs;
 mod m20250211_180236_songtitles;
 mod m20250211_181553_sets;
 mod m20250211_181843_performances;
+mod m20250223_115919_add_slug_to_countries;
 pub struct Migrator;
 
 #[async_trait::async_trait]
@@ -34,6 +35,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20250211_180236_songtitles::Migration),
             Box::new(m20250211_181553_sets::Migration),
             Box::new(m20250211_181843_performances::Migration),
+            Box::new(m20250223_115919_add_slug_to_countries::Migration),
             // inject-above (do not remove this comment)
         ]
     }
