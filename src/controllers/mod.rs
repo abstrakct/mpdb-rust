256fc061a842524b80e84f17c30d0dfd588e5daa
Rolf K <abstractstar@gmail.com>
2025-03-08 12:17:32 +0100

    Start adding a simple cities view

--- src/controllers/mod.rs
+++ src/controllers/mod.rs
@@ -1,14 +1,13 @@
 pub mod auth;
 
-pub mod cities;
-pub mod countries;
-pub mod venues;
-
 pub mod artists;
+pub mod cities;
 pub mod concerts;
+pub mod countries;
 pub mod performances;
 pub mod sets;
 pub mod songs;
 pub mod songtitles;
+pub mod venues;
 
 pub mod dashboard;
--- src/views/mod.rs
+++ src/views/mod.rs
@@ -1,2 +1,4 @@
 pub mod auth;
+
+pub mod cities;
 pub mod dashboard;
