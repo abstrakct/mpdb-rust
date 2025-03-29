b02f5758ea0848e9c2be1b55e616e2c43b9919b2
Rolf K <abstractstar@gmail.com>
2025-03-29 14:15:30 +0100

    Countries: add find_by_{name,slug}

--- src/models/countries.rs
+++ src/models/countries.rs
@@ -1,4 +1,5 @@
-pub use super::_entities::countries::{ActiveModel, Entity, Model};
+pub use super::_entities::countries::{self, ActiveModel, Entity, Model};
+use loco_rs::prelude::*;
 use sea_orm::entity::prelude::*;
 pub type Countries = Entity;
 
@@ -19,7 +20,41 @@ impl ActiveModelBehavior for ActiveModel {
 }
 
 // implement your read-oriented logic here
-impl Model {}
+impl Model {
+    /// Finds a country by the provided name
+    ///
+    /// # Errors
+    ///
+    /// When could not find country by the given name or DB query error
+    pub async fn find_by_name(db: &DatabaseConnection, name: &str) -> ModelResult<Self> {
+        let country = Countries::find()
+            .filter(
+                model::query::condition()
+                    .eq(countries::Column::Name, name)
+                    .build(),
+            )
+            .one(db)
+            .await?;
+        country.ok_or_else(|| ModelError::EntityNotFound)
+    }
+
+    /// Finds a country by the provided slug
+    ///
+    /// # Errors
+    ///
+    /// When could not find country by the given slug or DB query error
+    pub async fn find_by_slug(db: &DatabaseConnection, slug: &str) -> ModelResult<Self> {
+        let country = Countries::find()
+            .filter(
+                model::query::condition()
+                    .eq(countries::Column::Slug, slug)
+                    .build(),
+            )
+            .one(db)
+            .await?;
+        country.ok_or_else(|| ModelError::EntityNotFound)
+    }
+}
 
 // implement your write-oriented logic here
 impl ActiveModel {}
